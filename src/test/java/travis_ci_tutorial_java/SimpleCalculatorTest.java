6d9752e69877b54107a179ccbfbb9580f055114b
Jessie Clemons <36421867+jclemo2@users.noreply.github.com>
2019-04-01 15:55:59 -0400

    Update SimpleCalculatorTest.java
    Multiply, add, subtract, divide, cant divide by 0.

--- src/test/java/travis_ci_tutorial_java/SimpleCalculatorTest.java
+++ src/test/java/travis_ci_tutorial_java/SimpleCalculatorTest.java
@@ -9,6 +9,32 @@ public class SimpleCalculatorTest {
 	@Test
 	public void testAdd() {
 		SimpleCalculator calc = new SimpleCalculator();
-		assertEquals(calc.add(1, 1), 0);
+		assertEquals(calc.add(1, 1), 2);
+	}
+	@Test
+	public void testMinus() {
+		SimpleCalculator calc = new SimpleCalculator();
+		assertEquals(calc.subtract(5, 3), 2);
+	}
+	@Test
+	public void testMultiply() {
+		SimpleCalculator calc = new SimpleCalculator();
+		assertEquals(calc.multiply(1, 1), 1);
+	}
+	@Test
+	public void testDiv() {
+		SimpleCalculator calc = new SimpleCalculator();
+		assertEquals(calc.divide(1, 1), 1);
+	}
+	@Test
+	public void testDivide() {
+		SimpleCalculator calc = new SimpleCalculator();
+		//shouldn't be able to divide by zero
+		try {
+			calc.divide(2, 0);
+			fail("Should have thrown an exception!");
+		}
+		catch (ArithmeticException e) {
+		}
 	}
 }
